df59276f9792329c172cef044ada346a6a33a4af
Kinu <kinarithummar3007@gmail.com>
2026-05-20 16:55:42 +0530

    Added Rock-Paper-Scissors Game

---
+++ Level-1-Beginner-Arcade/Rock-paper_scissors.py
@@ -0,0 +1,42 @@
+﻿import random
+
+choices = ["rock", "paper", "scissors"]
+emoji = {
+    "rock": "✊",
+    "paper": "✋",
+    "scissors": "✌️"
+}
+
+while True:
+    user_choice = input("Enter your choice from (Rock ✊, Paper ✋, Scissors ✌️): ").lower()
+    if user_choice not in choices:
+        print("Invalid choice, Please Try Again !!")
+    else:
+        computer_choice = random.choice(choices)
+        if user_choice == "rock":
+            if computer_choice == "rock":
+                print(f"Computer also chose Rock {emoji['rock']}, It's a Tie !!")
+            elif computer_choice == "paper":
+                print(f"Computer chose Paper {emoji['paper']}, Computer Wins !!")
+            else:
+                print(f"Computer chose Scissors {emoji['scissors']}, You win !!")
+
+        elif user_choice == "paper":
+            if computer_choice == "rock":
+                print(f"Computer chose Rock {emoji['rock']}, You win !!")
+            elif computer_choice == "paper":
+                print(f"Computer also chose Paper {emoji['paper']}, It's a Tie !!")
+            else:
+                print(f"Computer chose Scissors {emoji['scissors']}, Computer Wins !!")      
+        
+        else:
+            if computer_choice == "rock":
+                print(f"Computer chose Rock {emoji['rock']}, Computer Wins !!")
+            elif computer_choice == "paper":
+                print(f"Computer chose Paper {emoji['paper']}, You win !!")
+            else:
+                print(f"Computer also chose Scissors {emoji['scissors']}, It's a Tie !!")
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != "yes":
+        print("Thanks for playing! Goodbye!")
+        break
